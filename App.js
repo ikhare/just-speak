aa84da168fb0ed642c82a82877141021b1837265
Indrajit Khare <ikhare@gmail.com>
2023-11-15 15:23:19 +0530

    Get the basic app up and working

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import "react-native-get-random-values";
+import { CONVEX_URL } from "@env";
+
+const convex = new ConvexReactClient(CONVEX_URL, {
+  unsavedChangesWarning: false,
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ConvexProvider client={convex}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ConvexProvider>
   );
 }
 
